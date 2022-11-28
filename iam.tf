e7d18ce4dcf0ff87fedfed03134cac534dd8d069
PrachiP29 <86628177+PrachiP29@users.noreply.github.com>
2022-11-28 11:26:25 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 # CREATE IAM ROLE FOR THE REPLICATION RULE
 resource "aws_iam_role" "replication01" {
-  name               = "tf-iam-role-replication-bucket-01"
-  provider           = aws.source
+  name = "tf-iam-role-replication-bucket-01"
+
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -19,9 +19,8 @@ resource "aws_iam_role" "replication01" {
 POLICY
 }
 resource "aws_iam_policy" "replication01" {
-  name     = "tf-iam-role-policy-replication-bucket-01"
-  provider = aws.source
-  policy   = <<POLICY
+  name   = "tf-iam-role-policy-replication-bucket-01"
+  policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -55,48 +54,12 @@ resource "aws_iam_policy" "replication01" {
       ],
       "Effect": "Allow",
       "Resource": "${aws_s3_bucket.buckettwo.arn}/*"
-    },
-    {
-      "Action": [
-        "kms:Decrypt"
-      ],
-      "Effect": "Allow",
-      "Condition": {
-        "StringLike": {
-          "kms:ViaService": "s3.eu-west-1.amazonaws.com",
-          "kms:EncryptionContext:aws:s3:arn": [
-            "${aws_s3_bucket.bucketone.arn}"
-          ]
-        }
-      },
-      "Resource": [
-        "${aws_kms_key.mykey.arn}"
-      ]
-    },
-    {
-      "Action": [
-        "kms:Encrypt"
-      ],
-      "Effect": "Allow",
-      "Condition": {
-        "StringLike": {
-          "kms:ViaService": "s3.eu-central-1.amazonaws.com",
-          "kms:EncryptionContext:aws:s3:arn": [
-            "${aws_s3_bucket.buckettwo.arn}"
-          ]
-        }
-      },
-      "Resource": [
-        "${aws_kms_key.prachikey.arn}"
-      ]
     }
   ]
 }
 POLICY
 }
-
 resource "aws_iam_role_policy_attachment" "replication01" {
-  provider   = aws.source
   role       = aws_iam_role.replication01.name
   policy_arn = aws_iam_policy.replication01.arn
 }
@@ -149,7 +112,7 @@ resource "aws_iam_policy" "replication02" {
         "${aws_s3_bucket.buckettwo.arn}/*"
       ]
     },
-    {
+     {
       "Action": [
         "s3:ReplicateObject",
         "s3:ReplicateDelete",
@@ -157,45 +120,12 @@ resource "aws_iam_policy" "replication02" {
       ],
       "Effect": "Allow",
       "Resource": "${aws_s3_bucket.bucketone.arn}/*"
-    },
-    {
-      "Action": [
-        "kms:Decrypt"
-      ],
-      "Effect": "Allow",
-      "Condition": {
-        "StringLike": {
-          "kms:ViaService": "s3.eu-central-1.amazonaws.com",
-          "kms:EncryptionContext:aws:s3:arn": [
-            "${aws_s3_bucket.buckettwo.arn}"
-          ]
-        }
-      },
-      "Resource": [
-        "${aws_kms_key.prachikey.arn}"
-      ]
-    },
-    {
-      "Action": [
-        "kms:Encrypt"
-      ],
-      "Effect": "Allow",
-      "Condition": {
-        "StringLike": {
-          "kms:ViaService": "s3.eu-west-1.amazonaws.com",
-          "kms:EncryptionContext:aws:s3:arn": [
-            "${aws_s3_bucket.bucketone.arn}"
-          ]
-        }
-      },
-      "Resource": [
-        "${aws_kms_key.mykey.arn}"
-      ]
     }
   ]
 }
 POLICY
 }
+
 resource "aws_iam_role_policy_attachment" "replication02" {
   provider   = aws.central
   role       = aws_iam_role.replication02.name
