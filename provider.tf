c6d9172b1b4835e761c02c9e832b74a536d42271
PrachiP29 <86628177+PrachiP29@users.noreply.github.com>
2022-12-02 19:27:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,18 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=4.45.0"
+       version = "~> 4.0"
     }
   }
 }
 provider "aws" {
   alias   = "source"
   region  = "eu-west-1"
+  version = "v2.70.0"
   profile = "PrachiAWS"
 }
 
 provider "aws" {
   alias   = "central"
   region  = "eu-central-1"
+  version = "v2.70.0"
   profile = "PrachiAWS"
 }
